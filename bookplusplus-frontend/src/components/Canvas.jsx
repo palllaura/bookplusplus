461ce8a33cd7054b6659c283d610c865bcd59a5a
palllaura <palllaura.tln@gmail.com>
2025-07-22 18:34:19 +0300

    Rotate book title + book drawing update

--- bookplusplus-frontend/src/components/Canvas.jsx
+++ bookplusplus-frontend/src/components/Canvas.jsx
@@ -1,33 +1,35 @@
 import '../App.css';
-import { useRef, useEffect } from 'react';
+import {useEffect, useRef} from 'react';
 
 const Canvas = () => {
 
-    const book = {
-        width: 60,
-        height: 120,
-        color: '#D9722C',
-        x: 100,
-        y: 100,
-        title: 'Book 1'
-    };
+    function createBook({width = 50, height = 120, color = '#D9722C', x = 100, y = 100, title = 'Untitled'}) {
+        return {width, height, color, x, y, title};
+    }
+
+    const book1 = createBook({x: 100, y: 100, title: 'Book 1'});
+    const book2 = createBook({x: 160, y: 100, title: 'Book 2', color: '#E84636'});
+    const book3 = createBook({width: 80, height: 140, color: '#FEB72BFF', x: 220, y: 80, title: 'Book 3'});
+
+    const books = [book1, book2, book3];
 
     const canvasRef = useRef(null);
 
     const draw = (ctx, width, height) => {
         ctx.clearRect(0, 0, width, height);
 
-        const bookWidth = book.width;
-        const bookHeight = book.height;
-        const x = book.x;
-        const y = book.y;
+        books.forEach(book => {
+            ctx.fillStyle = book.color;
+            ctx.fillRect(book.x, book.y, book.width, book.height);
 
-        ctx.fillStyle = book.color;
-        ctx.fillRect(x, y, bookWidth, bookHeight);
-
-        ctx.fillStyle = 'white';
-        ctx.font = '16px sans-serif';
-        ctx.fillText(book.title, x + 2, y + bookHeight / 2);
+            ctx.save();
+            ctx.translate(book.x + book.width / 2 + 8, book.y + book.height - 8);
+            ctx.rotate(-Math.PI / 2);
+            ctx.fillStyle = 'white';
+            ctx.font = '16px sans-serif';
+            ctx.fillText(book.title, 0, 0);
+            ctx.restore();
+        })
     };
 
     useEffect(() => {
@@ -48,7 +50,7 @@ const Canvas = () => {
         return () => window.removeEventListener('resize', resizeCanvas);
     }, []);
 
-    return <canvas className="canvas" ref={canvasRef} />;
+    return <canvas className="canvas" ref={canvasRef}/>;
 };
 
 export default Canvas;
