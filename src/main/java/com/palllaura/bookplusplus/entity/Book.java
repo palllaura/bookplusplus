3889987c7a4f5a3719f7ac887e4e7bac0531957e
palllaura <palllaura.tln@gmail.com>
2025-07-24 15:34:01 +0300

    Book entity update to match frontend

--- src/main/java/com/palllaura/bookplusplus/entity/Book.java
+++ src/main/java/com/palllaura/bookplusplus/entity/Book.java
@@ -5,16 +5,48 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Book {
-
+    /**
+     * Unique ID of each book.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    /**
+     * Book title.
+     */
     @Column(name = "title")
     private String title;
-
+    /**
+     * Book width in millimeters.
+     */
+    @Column(name = "width")
+    private int bookWidthInMm;
+    /**
+     * Book height in millimeters.
+     */
+    @Column(name = "height")
+    private int bookHeightInMm;
+    /**
+     * Color code of book.
+     */
+    @Column(name = "color")
+    private String color;
+    /**
+     * X coordinate of book position.
+     */
+    @Column(name = "x")
+    private int xPosition;
+    /**
+     * Y coordinate of book position.
+     */
+    @Column(name = "y")
+    private int yPosition;
 
 }
