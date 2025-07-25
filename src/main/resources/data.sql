fa2630ed6203d31a6846dc5a902c50a5863288ca
palllaura <palllaura.tln@gmail.com>
2025-07-25 13:10:26 +0300

    Add script for initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book (id, title, width, height, color, x, y)
+VALUES (1001, 'book 1', 50, 120, '#D9722C', 100, 100),
+       (1002, 'book 2', 60, 120, '#E84636', 160, 100),
+       (1003, 'book 3', 80, 140, '#ffb72b', 220, 80)
+ON CONFLICT (id) DO NOTHING;
