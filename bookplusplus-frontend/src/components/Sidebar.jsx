1fb0ed090f4890a3cf30b4e9ef32244223855d50
palllaura <palllaura.tln@gmail.com>
2025-07-31 12:06:15 +0300

    Sidebar: add save button

--- bookplusplus-frontend/src/components/Sidebar.jsx
+++ bookplusplus-frontend/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import logo from "../assets/logo.png";
+import save from "../assets/savebutton.png"
 import '../App.css';
 
 function Sidebar() {
@@ -6,8 +7,11 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <img src={logo} className="logo" alt="book++ logo"/>
-            <h3>"It’s my story so clearly I’m the hero."</h3>
-            <p>- Donatella</p>
+            <div>
+                <h3>"It’s my story so clearly I’m the hero."</h3>
+                <p>- Donatella</p>
+            </div>
+            <img src={save} className="save-button" alt="save button"/>
         </div>
     );
 }
