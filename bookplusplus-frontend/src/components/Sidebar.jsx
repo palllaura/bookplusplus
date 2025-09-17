b9ee9d6e90b56c20e96d3423169b7ab5769211f4
palllaura <palllaura.tln@gmail.com>
2025-09-17 11:41:09 +0300

    Sidebar: add edit button

--- bookplusplus-frontend/src/components/Sidebar.jsx
+++ bookplusplus-frontend/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import logo from "../assets/logo.png";
 import save from "../assets/savebutton.png"
 import reload from "../assets/reload.png"
+import edit from "../assets/edit.png"
 import '../App.css';
 
-function Sidebar({ onAddBook, onSave, onReload }) {
+function Sidebar({ onAddBook, onEdit, onSave, onReload }) {
 
     return (
         <div className="sidebar">
@@ -12,6 +13,11 @@ function Sidebar({ onAddBook, onSave, onReload }) {
                 className="logo"
                 alt="book++ logo"
                 onClick={onAddBook}
+            />
+            <img src={edit}
+                 className="button"
+                 alt="edit button"
+                 onClick={onEdit}
             />
             <img src={save}
                  className="button"
